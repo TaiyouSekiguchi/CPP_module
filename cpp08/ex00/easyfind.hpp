8c8f7a7092ad0a6f63642a141184e98ad10d12b4
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-07-07 23:31:50 +0900

    update

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,131 +1,26 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-#include <iostream>
 #include <algorithm>
-#include <vector>
-#include <list>
-#include <deque>
-#include <set>
-#include <map>
 
 template< typename T >
-typename T::const_iterator	easyfind( const T& ocean, int dia )
+typename T::const_iterator	easyfind(const T& ocean, int dia)
 {
-	typename T::const_iterator	ret = std::find( ocean.begin(), ocean.end(), dia );
-	if ( ret == ocean.end() )
-		throw ( "easyfind clould not find." );
+	typename T::const_iterator	ret = std::find(ocean.begin(), ocean.end(), dia);
+	if (ret == ocean.end())
+		throw ("easyfind clould not find.");
 
-	return ( ret );
-}
-
-template<>
-std::map<int, int>::const_iterator	easyfind<std::map<int, int> >( const std::map<int, int> & ocean, int dia )
-{
-	std::map<int, int>::const_iterator	ret = ocean.find( dia );
-	if ( ret == ocean.end() )
-		throw ( "easyfind clould not find." );
-
-	return ( ret );
+	return (ret);
 }
 
 template< typename T >
-void	test_easyfind( T& container )
+typename T::iterator	easyfind(T& ocean, int dia)
 {
-	container.push_back( 0 );
-	container.push_back( 42 );
-	container.push_back( 33 );
-	container.push_back( 9999 );
-	container.push_back( -522 );
+	typename T::iterator	ret = std::find(ocean.begin(), ocean.end(), dia);
+	if (ret == ocean.end())
+		throw ("easyfind clould not find.");
 
-	std::cout << "[ CONTAINER ]" << std::endl;
-	typename T::const_iterator	it = container.begin();
-	typename T::const_iterator	itEnd = container.end();
-	for ( ; it != itEnd; ++it )
-		std::cout << *it << " ";
-	std::cout << std::endl;
-
-	std::cout << "[ SEARCH ]" << std::endl;
-	std::cout << "42, 1031" << std::endl;
-
-	std::cout << "[ RESULT ]" << std::endl;
-	try
-	{
-		std::cout << *(easyfind( container, 42 )) << " is FOUND !!" << std::endl;
-		std::cout << *(easyfind( container, 1031 )) << " is FOUND !!" << std::endl;
-	}
-	catch ( const char * msg )
-	{
-		std::cerr << msg << std::endl;
-	}
-}
-
-template< typename T >
-void	test_easyfind_2( T & container )
-{
-	const int a[] = { 0, 42, 33, 9999, -522 };
-
-	T	tmp(a, a + 5);
-
-	container = tmp;
-
-	std::cout << "[ CONTAINER ]" << std::endl;
-	typename T::const_iterator	it = container.begin();
-	typename T::const_iterator	itEnd = container.end();
-	for ( ; it != itEnd; ++it )
-		std::cout << *it << " ";
-	std::cout << std::endl;
-
-	std::cout << "[ SEARCH ]" << std::endl;
-	std::cout << "42, 1031" << std::endl;
-
-	std::cout << "[ RESULT ]" << std::endl;
-	try
-	{
-		std::cout << *(easyfind( container, 42 )) << " is FOUNT !!" << std::endl;
-		std::cout << *(easyfind( container, 1031 )) << " is FOUNT !!" << std::endl;
-	}
-	catch ( const char * msg )
-	{
-		std::cerr << msg << std::endl;
-	}
-}
-
-template<>
-void	test_easyfind<std::map<int, int> >( std::map<int, int> & container )
-{
-	const std::pair<int, int> a[] = {
-		std::make_pair( 0, 0 ),
-		std::make_pair( 42, 1 ),
-		std::make_pair( 33, 2 ),
-		std::make_pair( 9999, 3 ),
-		std::make_pair( -522, 4 ),
-	};
-
-	std::map<int, int>	tmp(a, a + 5);
-
-	container = tmp;
-
-	std::cout << "[ CONTAINER ]" << std::endl;
-	std::map<int, int>::const_iterator	it = container.begin();
-	std::map<int, int>::const_iterator	itEnd = container.end();
-	for ( ; it != itEnd; ++it )
-		std::cout << "[ " <<  it->first << " : " << it->second << " ]  ";
-	std::cout << std::endl;
-
-	std::cout << "[ SEARCH ]" << std::endl;
-	std::cout << "42, 1031" << std::endl;
-
-	std::cout << "[ RESULT ]" << std::endl;
-	try
-	{
-		std::cout << easyfind( container, 42 )->second << " is FOUND !!" << std::endl;
-		std::cout << easyfind( container, 1031 )->second << " is FOUND !!" << std::endl;
-	}
-	catch ( const char * msg )
-	{
-		std::cerr << msg << std::endl;
-	}
+	return (ret);
 }
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,64 +1,135 @@
 #include <iostream>
 #include <vector>
 #include <list>
-#include <set>
-#include <map>
+#include <deque>
 #include "easyfind.hpp"
 
-int	main( void )
+void	TestEasyFindVector(void)
 {
-	std::cout << "############## VECTOR PART ###############" << std::endl;
-	std::vector<int>	intVec;
-	test_easyfind( intVec );
+	std::cout << "TEST easyfind for vector : ";
 
-	std::cout << "\n############## LIST PART #################" << std::endl;
-	std::list<int>	intList;
-	test_easyfind( intList );
+	std::vector<int>	container;
 
-	std::cout << "\n############## DEQUE PART ################" << std::endl;
-	std::deque<int>	intDeque;
-	test_easyfind( intDeque );
+	container.push_back( 0 );
+	container.push_back( 42 );
+	container.push_back( 33 );
+	container.push_back( 9999 );
+	container.push_back( -522 );
 
-	std::cout << "\n############## SET PART ##################" << std::endl;
-	std::set<int>	intSet;
-	test_easyfind_2( intSet );
-
-	std::cout << "\n############## MULTISET PART #############" << std::endl;
-	std::multiset<int>	intMultiSet;
-	test_easyfind_2( intMultiSet );
-
-	std::cout << "\n############## MAP PART ##################" << std::endl;
-	std::map<int ,int>	intMap;
-	test_easyfind( intMap );
-
-	//VECTOR (STRING) compile error
-	/*
-	std::vector<std::string>	str_vector;
-
-	str_vector.push_back( "42tokyo" );
-	str_vector.push_back( "Born2cord" );
-	str_vector.push_back( "tsekiguc" );
-
-	std::cout << "[ str_vector ]" << std::endl;
-	for ( size_t i = 0; i < SIZE; ++i )
-	{
-		if ( i != SIZE - 1 )
-			std::cout << str_vector.at( i ) << ", ";
-		else
-			std::cout << str_vector.at( i ) << std::endl;
-	}
-
-	std::cout << "[ RESULT ]" << std::endl;
 	try
 	{
-		std::cout << *(easyfind( str_vector, "42tokyo" )) << std::endl;
-		std::cout << *(easyfind( str_vector, "notfound" )) << std::endl;
+		easyfind(container, 0);
+		easyfind(container, 42);
+		easyfind(container, 33);
+		easyfind(container, 9999);
+		easyfind(container, -522);
 	}
-	catch ( const char * msg )
+	catch(const char* msg)
 	{
 		std::cerr << msg << std::endl;
 	}
-	*/
+
+	try
+	{
+		easyfind(container, 1031);
+		std::cerr << "[ NG ]" << std::endl;
+	}
+	catch(const char* msg)
+	{
+		;
+	}
+
+	std::cout << "[ OK ]" << std::endl;
+
+	return ;
+}
+
+void	TestEasyFindList(void)
+{
+	std::cout << "TEST easyfind for list : ";
+
+	std::list<int>	container;
+
+	container.push_back( 0 );
+	container.push_back( 42 );
+	container.push_back( 33 );
+	container.push_back( 9999 );
+	container.push_back( -522 );
+
+	try
+	{
+		easyfind(container, 0);
+		easyfind(container, 42);
+		easyfind(container, 33);
+		easyfind(container, 9999);
+		easyfind(container, -522);
+	}
+	catch(const char* msg)
+	{
+		std::cerr << msg << std::endl;
+	}
+
+	try
+	{
+		easyfind(container, 1031);
+		std::cerr << "[ NG ]" << std::endl;
+	}
+	catch(const char* msg)
+	{
+		;
+	}
+
+	std::cout << "[ OK ]" << std::endl;
+
+	return ;
+}
+
+void	TestEasyFindDeque(void)
+{
+	std::cout << "TEST easyfind for deque : ";
+
+	std::deque<int>	container;
+
+	container.push_back( 0 );
+	container.push_back( 42 );
+	container.push_back( 33 );
+	container.push_back( 9999 );
+	container.push_back( -522 );
+
+	try
+	{
+		easyfind(container, 0);
+		easyfind(container, 42);
+		easyfind(container, 33);
+		easyfind(container, 9999);
+		easyfind(container, -522);
+	}
+	catch(const char* msg)
+	{
+		std::cerr << msg << std::endl;
+	}
+
+	try
+	{
+		easyfind(container, 1031);
+		std::cerr << "[ NG ]" << std::endl;
+	}
+	catch(const char* msg)
+	{
+		;
+	}
+
+	std::cout << "[ OK ]" << std::endl;
+
+	return ;
+}
+
+
+int	main( void )
+{
+	TestEasyFindVector();
+	TestEasyFindList();
+	TestEasyFindDeque();
 
 	return ( 0 );
 }
